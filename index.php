23e98775f04cb791b271c6a8f1a19c2dcb51da28
Carson Fleming <cflems@cflems.net>
2022-09-14 21:00:54 -0700

    Basic UI

--- index.php
+++ index.php
@@ -6,16 +6,29 @@ define('INTERNAL', 1);
   <head>
     <meta charset="utf-8" />
     <title>ucflems</title>
-    <link rel="stylesheet" href="https://cflems.net/css/nav.css" />
     <link rel="stylesheet" href="https://cflems.net/css/energize.css" />
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cflems.net/css/contact.css" />
+    <link rel="stylesheet" href="https://cflems.net/css/nav.css" />
+    <link rel="stylesheet" href="local.css" />
   </head>
   <body>
 <?php
 @require('/home/cflems/site/cflems.net/shared/nav.php');
 ?>
   <div id="part1">
+    <div id="bubble">
+      <h1>ucflems</h1>
+      <hr />
+      <div>
+        content
+      </div>
+      <hr />
+      <div id="bubblefoot">
+        <a href="https://cflems.net/">&larr; Home</a>
+      </div>
+    </div>
     content..
   </div>
+  <p id="copy"><span id="copynotice">Copyright &copy; 2016-<?=date('Y');?> Carson Fleming</span></p>
   </body>
 </html>
